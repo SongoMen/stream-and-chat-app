fce450a32f8402c136284a6833b52490b87e9ff8
SongoMen <songomengaming@gmail.com>
2021-01-04 02:06:45 +0100

    added player container

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Chat from "./chat"
+import Player from "./player"
 
 const Home = () => {
   return (
     <div className="home">
+      <Player />
       <Chat />
     </div>
   );
